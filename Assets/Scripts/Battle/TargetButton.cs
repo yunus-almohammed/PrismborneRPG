11166e7fe8a3750b7e1dee1d10f8b41de6efd4d0
Yunus Mohammed <yunusalmohammed@gmail.com>
2026-05-24 22:01:47 +0300

    Add target button helper script

---
+++ Assets/Scripts/Battle/TargetButton.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetButton : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private BattleUnitView unitView;
+
+    private BattleManager battleManager;
+    private int battleUnitIndex = -1;
+
+    public void Setup(BattleManager manager, int unitIndex)
+    {
+        battleManager = manager;
+        battleUnitIndex = unitIndex;
+
+        if (button == null)
+        {
+            button = GetComponent<Button>();
+        }
+
+        if (button == null)
+        {
+            return;
+        }
+
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => battleManager.BasicAttackTarget(battleUnitIndex));
+    }
+
+    public void SetInteractable(bool value)
+    {
+        if (button != null)
+        {
+            button.interactable = value;
+        }
+    }
+}
